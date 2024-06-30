8dab9845c2d4a3d52739fd7c6e4c52c72158abcd
shroom <me@shro.me>
2024-06-30 23:11:10 +0100

    big huge new updates. too bad they don't work when built

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,6 +1,85 @@
-import { coreEvents, start } from '../core/core.ts'
+import { coreEvents, start } from '../core/core.js'
 import ora from 'ora'
-import cliProgress from 'cli-progress'
+import 'colors'
+import readline from 'readline-sync'
+
+console.log('bookbuddies'.bgMagenta.bold)
+console.log(
+	'Participate in the Community Reflector by the LBRY Foundation'.magenta
+)
+
+const settingsFile = Bun.file('settings.json')
+if (!(await settingsFile.exists())) {
+	console.log(
+		"\nWelcome to bookbuddies! Let's get some initial settings sorted.\nYou can always change these while bookbuddies is running.\n"
+			.yellow
+	)
+	const settings: {
+		storageSpace: number
+		username: string
+		advanced: { rpcPort: number; udpPort: number; tcpPort: number }
+	} = {
+		storageSpace: 10,
+		username: '',
+		advanced: {
+			rpcPort: 5279,
+			udpPort: 4444,
+			tcpPort: 4444
+		}
+	}
+	settings.storageSpace = readline.questionInt(
+		'How much storage should bookbuddies use? (in GB) '.blue +
+			'[default: 10] '.gray,
+		{
+			defaultInput: '10'
+		}
+	)
+	settings.username = readline.question(
+		"Would you like to provide a name? This is only for the LBRY Foundation, to see who's contributing. "
+			.blue + '[optional] '.gray,
+		{
+			defaultInput: ''
+		}
+	)
+	if (
+		readline.keyInYN(
+			'Do you want to set advanced LBRY SDK settings? '.blue + '[y/n] '.gray,
+			{
+				guide: false
+			}
+		)
+	) {
+		settings.advanced.rpcPort = readline.questionInt(
+			'What port should the LBRY SDK use for JSON-RPC? '.blue +
+				'[default: 5279] '.gray,
+			{
+				min: 1024,
+				max: 65535,
+				defaultInput: '5279'
+			}
+		)
+		settings.advanced.udpPort = readline.questionInt(
+			'What port should the LBRY SDK use for UDP communication for seeding? '
+				.blue + '[default: 4444] '.gray,
+			{
+				min: 1024,
+				max: 65535,
+				defaultInput: '4444'
+			}
+		)
+		settings.advanced.tcpPort = readline.questionInt(
+			'What port should the LBRY SDK use for TCP communication for seeding? '
+				.blue + '[default: 4444] '.gray,
+			{
+				min: 1024,
+				max: 65535,
+				defaultInput: '4444'
+			}
+		)
+	}
+
+	console.log(settings)
+}
 
 let currentSpinner = ora('Checking for the LBRY SDK').start()
 
@@ -9,7 +88,20 @@ coreEvents.on('lbrynet exists', (exists) => {
 	currentSpinner.text = 'Downloading the LBRY SDK'
 })
 coreEvents.on('lbrynet installed', () =>
-	currentSpinner.succeed('Downloaded the LBRY SDK')
+	currentSpinner.succeed('Installed the LBRY SDK')
 )
+coreEvents.on('lbrynet downloaded', () => {
+	currentSpinner.text = 'Unpacking the LBRY SDK'
+	currentSpinner.color = 'yellow'
+})
+coreEvents.on('lbrynet starting', () => {
+	currentSpinner = ora('Starting the LBRY SDK').start()
+})
+coreEvents.on('lbrynet start', () => {
+	currentSpinner.succeed('Started the LBRY SDK')
+})
+coreEvents.on('lbrynet start error', () => {
+	currentSpinner.fail('Failed to start the LBRY SDK')
+})
 
 await start()
--- src/core/core.ts
+++ src/core/core.ts
@@ -1,15 +1,17 @@
 import JSZip from 'jszip'
 import EventEmitter from 'events'
+import fs from 'fs/promises'
+import { Readable } from 'stream'
 
 const eventEmitter = new EventEmitter()
-const start = async () => {
+
+const ensureLBRYSDK = async () => {
 	// Check if the LBRY SDK for this platform is installed in the cwd.
 	// If not, download it and save to the correct location.
 	// The LBRY SDK is lbrynet for Linux and macOS, and lbrynet.exe for Windows.
 
-	const file = Bun.file(
-		process.platform === 'win32' ? 'lbrynet.exe' : 'lbrynet'
-	)
+	const path = 'lbrynet' + (process.platform === 'win32' ? '.exe' : '')
+	const file = Bun.file(path)
 	if (!(await file.exists())) {
 		eventEmitter.emit('lbrynet exists', false)
 		const lbrynetBuffer = await (
@@ -19,18 +21,50 @@ const start = async () => {
 		).arrayBuffer()
 		const zip = new JSZip()
 		await zip.loadAsync(lbrynetBuffer)
-		const lbrynet = zip
-			.file('lbrynet' + (process.platform === 'win32' ? '.exe' : ''))
-			?.nodeStream()
+		const lbrynet = zip.file(path)?.nodeStream()
+		eventEmitter.emit('lbrynet downloaded')
 		if (!lbrynet) throw new Error('Failed to download the LBRY SDK.')
 
-		const chunks: Buffer[] = []
-		lbrynet.on('data', (chunk) => chunks.push(chunk))
-		lbrynet.on('end', async () => {
-			await Bun.write(file, Buffer.concat(chunks))
-			eventEmitter.emit('lbrynet installed')
+		await new Promise<void>((resolve, reject) => {
+			const chunks: Buffer[] = []
+			lbrynet.on('data', (chunk) => chunks.push(chunk))
+			lbrynet.on('end', async () => {
+				await Bun.write(file, Buffer.concat(chunks))
+				await fs
+					.chmod(path, 0o755)
+					.catch(() => console.log('Failed to make the LBRY SDK executable.'))
+				eventEmitter.emit('lbrynet installed')
+				resolve()
+			})
 		})
 	} else eventEmitter.emit('lbrynet exists', true)
+}
+
+const start = async () => {
+	await ensureLBRYSDK()
+
+	eventEmitter.emit('lbrynet starting')
+
+	// Run the LBRY SDK
+	const sdkInstance = Bun.spawn([`./lbrynet`, 'start'], {
+		stderr: 'pipe'
+	})
+	if (!sdkInstance.stderr)
+		throw new Error('Failed to start the LBRY SDK: no stderr pipe')
+
+	const outputDecoder = new TextDecoder()
+	// @ts-ignore
+	for await (const line of sdkInstance.stderr) {
+		const lineString = outputDecoder.decode(line).trim()
+		if (lineString.includes('RPC server failed to bind TCP')) {
+			eventEmitter.emit('lbrynet start error', true)
+			break
+		}
+		if (lineString.includes('on-demand fetching height 0')) {
+			eventEmitter.emit('lbrynet start', true)
+			break
+		}
+	}
 }
 
 export { eventEmitter as coreEvents, start }
