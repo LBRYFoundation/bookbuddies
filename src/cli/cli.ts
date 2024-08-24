400410ff7f939be3c55005f31106e53033eb3c5d
ezShroom <68300730+ezShroom@users.noreply.github.com>
2024-08-24 20:43:57 +0200

    more files without prettier prettier'd

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -14,7 +14,8 @@ const config = await getOrCreateConfig()
 import * as Sentry from '@sentry/bun'
 import commitHash from '../common/commit-hash.ts' with { type: 'macro' }
 import sentryDsn from '../common/sentry-dsn.ts' with { type: 'macro' }
-if (config.sentry && typeof sentryDsn !== 'undefined' && sentryDsn !== null) Sentry.init({ dsn: sentryDsn, release: commitHash })
+if (config.sentry && typeof sentryDsn !== 'undefined' && sentryDsn !== null)
+	Sentry.init({ dsn: sentryDsn, release: commitHash })
 
 let currentSpinner = ora({
 	text: 'Checking for the LBRY SDK',
